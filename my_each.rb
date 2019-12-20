440aec92e72fa904dababb238ebb08d1bee8a55d
Austin Johnson <technicallyaustin@gmail.com>
2019-12-20 19:49:50 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+ counter = 0 
+ while array.count > counter do
+   i = array[counter]
+   yield i
+ counter += 1 
+ end
+ p array
 end
+ 
+ #my_each(array) {p "#{i} plus some words"}
