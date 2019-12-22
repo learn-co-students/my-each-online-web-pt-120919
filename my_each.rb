643616b0fd5e716e8e233f5174026f3498a73ccf
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 01:38:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array. index)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+my_each(list) {|i| puts "So I think #{i} is a piece of cake"}
+
