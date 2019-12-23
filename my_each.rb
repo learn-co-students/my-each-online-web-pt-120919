0c699b6ee34f7edad93c9af043c68df9330a9b77
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-23 04:29:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,28 @@
-def my_each # put argument(s) here
-  # code here
+
+list = ["arel", "jon", "logan","spencer"]
+
+def my_each(array)
+  if block_given?
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  i[2]
 end
+end
+
+my_each(list) {|i| "So I think #{i} is weird."}
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} want to hang?"}
